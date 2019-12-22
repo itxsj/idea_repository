c8a2260a23c4b18ec3c6ff17f3f312139e3b5c2c
xsj <473423451@qq.com>
2019-12-22 20:44:03 +0800

    右侧修改testController

--- src/main/java/com/offcn/controller/TestController.java
+++ src/main/java/com/offcn/controller/TestController.java
@@ -3,8 +3,7 @@ package com.offcn.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("这是右侧打印的测试---");
-    }
-    public static void main(String[] args) {
         System.out.println("这是左侧的打印测试。。。。");
     }
+
 }
